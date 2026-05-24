f5779bfe92f3b6501a426157be796bbe017809b4
Victor YE <victorye@outlook.fr>
2026-05-24 16:51:26 +0200

    MAJ Affichage pour avoir le chemin des fichiers

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from app.parser.file_loader import *
 
-p = r"e:\workspace\git_workspace\CodeAgent"
+p = Path("e:\workspace\git_workspace\CodeAgent")
 
 for i in read_dir(p):
-    print(i["path"].name)
+    print(i["path"].relative_to(p.parent))
